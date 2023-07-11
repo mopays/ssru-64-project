090a26496a2a25f90e0f79aa61d0520175ac87c9
mopays <75215491+mopays@users.noreply.github.com>
2023-07-11 13:30:10 +0700

    folk
    add derails page

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -14,25 +14,9 @@ function Details() {
 
 
       <blockquote class="text-xl italic font-semibold text-center text-gray-900 dark:text-white mt-3">
-        <h1>t</h1>
-        <h1>t</h1>
-        <h1>t</h1>
-        <h1>t</h1>
-        <h1>t</h1>
-        <h1>t</h1>
-        <h1>t</h1>
-        <h1>t</h1>
-        <h1>t</h1>
-        <h1>t</h1>
-        <h1>t</h1>
-        <h1>t</h1>
-        <h1>t</h1>
-        <h1>t</h1>
-        <h1>t</h1>
-        <h1>t</h1>
-        <h1>t</h1>
-        <h1>t</h1>
-        <h1>t</h1>
+        <h1>price :</h1>
+        <h1>details :</h1>
+        <h1>tag</h1>
       </blockquote>
       <div class="text-center  content-center mt-4  "> 
         <button  type="button" class="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:focus:ring-yellow-900">Yellow</button>
