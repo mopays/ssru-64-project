6743a9afb1c5c575721e1335217e39cd02fcd072
mopays <75215491+mopays@users.noreply.github.com>
2023-07-11 13:22:47 +0700

    Update Details.js

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -2,7 +2,39 @@ import React from 'react'
 
 function Details() {
   return (
-    <div>Details</div>
+    <div>
+
+      <blockquote class="text-xl italic font-semibold text-center text-gray-900 dark:text-white">
+          <p>Name</p>
+      </blockquote>
+
+
+      <img class="h-auto max-w-lg mx-auto" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png" alt="image description"/>
+
+
+
+      <blockquote class="text-xl italic font-semibold text-center text-gray-900 dark:text-white">
+        <h1>t</h1>
+        <h1>t</h1>
+        <h1>t</h1>
+        <h1>t</h1>
+        <h1>t</h1>
+        <h1>t</h1>
+        <h1>t</h1>
+        <h1>t</h1>
+        <h1>t</h1>
+        <h1>t</h1>
+        <h1>t</h1>
+        <h1>t</h1>
+        <h1>t</h1>
+        <h1>t</h1>
+        <h1>t</h1>
+        <h1>t</h1>
+        <h1>t</h1>
+        <h1>t</h1>
+        <h1>t</h1>
+      </blockquote>
+    </div>
   )
 }
 
